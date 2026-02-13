516eb7796047d84fe44d03f80df7e697b08f5dca
kfischi <kfir.biz@gmail.com>
2026-02-13 13:36:31 +0000

    Fix client.ts with hardcoded values

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://ulfwxmjerugxayuyliug.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZnd4bWplcnVneGF5dXlsaXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5MTIyODAsImV4cCI6MjA1MTQ4ODI4MH0.ZjWpPuONqc1o7LXYnXvGI8hWUm2wvgvh-5tDkf6aGMQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
