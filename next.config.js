ac7b34e6ca945167eb54d3ad739254c889a1c535
kfischi <kfir.biz@gmail.com>
2026-02-13 07:07:06 +0000

    Add tours.tzimer360.co.il to image domains

--- next.config.js
+++ next.config.js
@@ -12,11 +12,15 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/decirk3zb/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tours.tzimer360.co.il',
+        pathname: '/Images/**',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   compress: true,
 };
-
 module.exports = nextConfig;
